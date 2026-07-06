b0b53cc28206c792bcb9bbf29b51b6a987ee292e
RST-CODE <bamaticha@gmail.com>
2026-07-06 15:49:25 +0700

    Create ga.js

---
+++ ga.js
@@ -0,0 +1,13 @@
+// Google tag (gtag.js) - G-1G42D485D0
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-1G42D485D0';
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', 'G-1G42D485D0');
+})();
